acc181faa85845c96fd4c6544ed5bc7f6c4fe1ab
beayancan <beayancan@uc.cl>
2023-07-02 15:44:56 -0400

    feat(client): router implementation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,25 @@
-import React, { useState } from 'react';
-import { Login } from './components/login';
-import { MainView } from './components/main';
+import { useState } from 'react';
+import { Login } from './pages/Login';
+import { MainView } from './pages/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { NewPomodoro } from './pages/NewPomodoro';
+import { EditPomodoro } from './pages/EditPomodoro';
 
 export const App = () => {
   const [userInfo, setUserInfo] = useState({ id: '', email: '' });
 
   return (
-    <>
-      {(!userInfo || !userInfo.email) && <Login setUserInfo={setUserInfo} />}
-      {(userInfo.email) && <MainView userId={userInfo.id} userEmail={userInfo.email} />}
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login setUserInfo={setUserInfo} />} />
+        <Route path="/main" element={(userInfo.email) ? <MainView userId={userInfo.id} userEmail={userInfo.email} /> : <p>error</p>} />
+        <Route path="/pomodoro/create" element={<NewPomodoro userId={userInfo.id} />} />
+        <Route path="/pomodoro/:pomodoroId" element={<EditPomodoro userEmail={userInfo.email} />} />
+      </Routes>
+    </Router>
   );
 }
